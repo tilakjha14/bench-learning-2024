840052ca40ca0f3236e8d2d925eed24dbd774367
tilakjha14 <tilakjha14@gmail.com>
2024-04-23 11:17:47 +0530

    exercise 1
    exercise 1 test cases

--- exercises/exercise1/test/test.js
+++ exercises/exercise1/test/test.js
@@ -9,10 +9,40 @@ describe("Arithmetic API", () => {
   it("should add two numbers", (done) => {
     chai
       .request(app)
-      .get("/api/calculate/add?a=5&b=3")
+      .get("/api/calculate/addition?a=5&b=3")
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.result).to.equal(8);
+        done();
+      });
+  });
+  it("should substract two numbers", (done) => {
+    chai
+      .request(app)
+      .get("/api/calculate/substraction?a=5&b=3")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.result).to.equal(2);
+        done();
+      });
+  });
+  it("should multiply two numbers", (done) => {
+    chai
+      .request(app)
+      .get("/api/calculate/multiplication?a=5&b=3")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.result).to.equal(15);
+        done();
+      });
+  });
+  it("should divide two numbers", (done) => {
+    chai
+      .request(app)
+      .get("/api/calculate/division?a=6&b=3")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.result).to.equal(2);
         done();
       });
   });
