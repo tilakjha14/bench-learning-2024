3178b29c0afee6114c94ff58f397a283d9610a49
tilakjha14 <tilakjha14@gmail.com>
2024-05-16 12:12:02 +0530

    MonfoDB - querying documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = "mongodb://127.0.0.1:27017";
 
@@ -17,37 +17,69 @@ const databaseName = "task-manager";
 async function run() {
   try {
     //connect the client to server
-    const clientConnected = await client.connect();
+    // const clientConnected = await client.connect();
 
-    //Send a ping to confirm successful connection
-    const db = clientConnected.db(databaseName);
+    // //Send a ping to confirm successful connection
+    // const db = clientConnected.db(databaseName);
 
-    //const users = db.collection("users");
+    // const users = db.collection("users");
     // const doc = {
-    //   name: "Tilak Jha",
+    //   name: "Test",
     //   age: 31,
     // };
     // const result = await users.insertOne(doc);
-    const tasks = db.collection("tasks");
-    const doc = [
-      {
-        task: "Task 1",
-        isCompleted: true,
-      },
-      {
-        task: "Task 2",
-        isCompleted: true,
-      },
-      {
-        task: "Task 3",
-        isCompleted: false,
-      },
-    ];
+    // console.log(
+    //   "Inserted document with _id: =" + JSON.stringify(result.insertedId)
+    // );
+    // const tasks = db.collection("tasks");
+    // const doc = [
+    //   {
+    //     task: "Task 1",
+    //     isCompleted: true,
+    //   },
+    //   {
+    //     task: "Task 2",
+    //     isCompleted: true,
+    //   },
+    //   {
+    //     task: "Task 3",
+    //     isCompleted: false,
+    //   },
+    // ];
 
-    const result = await tasks.insertMany(doc);
-    console.log(
-      "Inserted document with _id: =" + JSON.stringify(result.insertedIds)
-    );
+    // const result = await tasks.insertMany(doc);
+    // console.log(
+    //   "Inserted document with _id: =" + JSON.stringify(result.insertedIds)
+    // );
+
+    // db.collection("tasks").findOne({ task: "Task 1" }, (error, result) => {
+    //   if (error) {
+    //     return console.log("error=", error);
+    //   }
+    //   console.log("Result=", result);
+    // });
+    //db.collection.findOne(query, projection, options);
+
+    // await client.connect();
+    // const database = client.db("task-manager");
+    // const collection = database.collection("tasks");
+    // const query = { isCompleted: true };
+    // //  const result = await collection.findOne(query);
+
+    // const result = await collection.find(query).next();
+
+    await client.connect();
+    const database = client.db("task-manager");
+    const collection = database.collection("tasks");
+    const query1 = {
+      _id: new ObjectId("664323a94208546e56a6824f"),
+    };
+    const result = await collection.findOne(query1);
+
+    const result2 = await collection.find({ isCompleted: false }).toArray();
+
+    // Log the result
+    console.log("result=", result, " result2=", result2);
   } finally {
     // Ensures that the client will close when you finish/error
 
